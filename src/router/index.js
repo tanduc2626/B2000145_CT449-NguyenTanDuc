501610773d558f806831de56c1cc0bffafc12d9a
tanduc2626 <ducb2000145@student.ctu.edu.vn>
2024-04-04 10:24:24 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,12 @@ import ContactBook from "@/views/ContactBook.vue";
 
 const routes = [
 {
-    path: "/:pathMatch(.*)*",
-    name: "notfound",
-    component: () => import("@/views/NotFound.vue"),
-    },
-    ];
+    path: "/contacts/:id",
+name: "contact.edit",
+component: () => import("@/views/ContactEdit.vue"),
+props: true // Truyền các biến trong $route.params vào làm props
+},
+];
 const router = createRouter({
 history: createWebHistory(import.meta.env.BASE_URL),
 routes,
