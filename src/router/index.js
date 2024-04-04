b8bc9ef968238d96b2115faec30b09db522b4eb0
tanduc2626 <ducb2000145@student.ctu.edu.vn>
2024-04-04 09:53:04 +0700

    Cai dat trang loi 404

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createWebHistory, createRouter } from "vue-router";
+import ContactBook from "@/views/ContactBook.vue";
+
+const routes = [
+{
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+    },
+    ];
+const router = createRouter({
+history: createWebHistory(import.meta.env.BASE_URL),
+routes,
+});
+export default router;
